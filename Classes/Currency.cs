4ac67a39fd9ec6770589afb3f8ca4a8abe88794d
Ricardo Lourenço <ric.filip85@hotmail.com>
2020-03-23 17:46:38 +0000

    Project Creation

---
+++ Classes/Currency.cs
@@ -0,0 +1,16 @@
+﻿namespace Classes
+{
+    using Newtonsoft.Json;
+
+    public class Currency
+    {
+        [JsonProperty("code")]
+        public string Code { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("symbol")]
+        public string Symbol { get; set; }
+    }
+}
---
+++ Classes/RegionalBloc.cs
@@ -0,0 +1,20 @@
+﻿namespace Classes
+{
+    using System.Collections.Generic;
+    using Newtonsoft.Json;
+
+    public class RegionalBloc
+    {
+        [JsonProperty("acronym")]
+        public string Acronym { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("otherAcronyms")]
+        public List<string> OtherAcronyms { get; set; }
+
+        [JsonProperty("otherNames")]
+        public List<string> OtherNames { get; set; }
+    }
+}
